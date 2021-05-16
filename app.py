30cf708b51b931a68bdf26c9c8dfcafa0b853b98
Artem Shuliak <artemshuliak@gmail.com>
2021-05-16 11:07:24 +0300

    initial commit to heroku

---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session
+import os 
+from werkzeug.utils import secure_filename
+from rq import Queue
+from worker import r
+from neural_model_task import background_task
+
+#where our images will go
+upload_folder = 'image_uploads'
+#what filetypes are allowed
+allowed_extensions = {'png', 'jpg', 'jpeg'}
+
+#create our ap
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+q = Queue(connection=r)
+
+#load files to our upload folder
+app.config['upload_folder'] = upload_folder
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+model_progress = 0
+global job_id
+
+@app.route('/', methods = ["POST", "GET"])
+def home():
+    
+    for file in os.scandir('image_uploads/'):
+        if file.name != '.gitkeep':
+            os.remove(file)
+
+    for file in os.scandir('static/result_images/'):
+        if file.name != '.gitkeep':
+            os.remove(file)
+            
+    return render_template('home.html')
+
+
+@app.route('/progress')
+def progres():
+    # print(model_progress)
+    global job_id 
+    print(job_id)
+    response = q.fetch_job(job_id)
+    if response.result == None: 
+        return jsonify(response='none')
+    if response.result != None:
+        session['result_image'] = response.result
+        return jsonify(response='some', redirect=url_for("result"))
+     
+     
+@app.route('/result')
+def result():
+    response_image = session['result_image']
+    return render_template('result.html', image_name = response_image)
+
+
+@app.route('/upload', methods = ["POST", "GET"])
+def upload():
+    
+    if request.method == "POST":
+        
+        base_photo = request.files['base_photo']
+        style_photo = request.files['style_photo']
+   
+        if allowed_file(base_photo.filename) and allowed_file(style_photo.filename):
+            
+            base_photo_filename = secure_filename(base_photo.filename) 
+            base_photo.save(os.path.join(app.config['upload_folder'], base_photo_filename))
+            base_photo_filepath = os.path.join(app.config['upload_folder'], base_photo_filename)
+            result_photo_filename = base_photo_filename
+            
+            style_photo_filename = secure_filename(style_photo.filename) 
+            style_photo.save(os.path.join(app.config['upload_folder'], style_photo_filename))
+            style_photo_filepath = os.path.join(app.config['upload_folder'], style_photo_filename)
+            print(base_photo_filepath)
+            
+            job = q.enqueue(background_task, base_photo_filepath, style_photo_filepath, result_photo_filename)
+            global job_id
+            job_id = job.id
+            return jsonify(reponse='sucess') 
+
+    return jsonify(reponse='no upload')
+        
+if __name__ == '__main__':
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+    app.run(debug=True)
+    
+    
+    
---
+++ neural_model_task.py
@@ -0,0 +1,12 @@
+from neural_net import nst_model
+
+def background_task(base_photo_filepath, style_photo_filepath, result_photo_filename):
+    def callback(progress):
+        global model_progress
+        model_progress = progress
+        print(progress)
+                
+    model = nst_model(base_photo_filepath, style_photo_filepath, result_photo_filename)
+    print('started function')
+    fname = model.train_net(30, callback)
+    return fname
